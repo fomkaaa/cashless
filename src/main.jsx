32329e036fc9ccb501276f6011e6d2b21dcb2cb9
fomkaaa <faa6002@gmail.com>
2025-11-30 16:06:31 +0200

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// 🔥 Відновлення шляху після редіректу GitHub Pages
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/">
     <App />
-  </StrictMode>,
-)
-
+  </BrowserRouter>
+);
